fe503d4d35feb1e965b20206f1f6d6234c769a78
Charlie (Mac) <2712cgc@gmail.com>
2023-09-18 13:41:49 +0100

    Setup the main file

--- source/main.lua
+++ source/main.lua
@@ -1,1 +1,19 @@
--- Placeholder until the project takes off
+-- PlayDate Core Libraries
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+-- GitHub Libraries
+import "scripts/libraries/AnimatedSprite"
+import "scripts/libraries/LDtk"
+
+-- PlayDate Constants
+local pd <const> = playdate
+local gfx <const> = playdate.graphics
+
+-- Main Game Loop
+function pd.update()
+    gfx.sprite.update()
+    pd.timer.updateTimers()
+end
