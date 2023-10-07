c4458f1e8e9af0a160733977f7b548f01abd9ea0
Charlie (Mac) <2712cgc@gmail.com>
2023-10-07 11:23:34 +0100

    Movement code more robust

--- source/scripts/player.lua
+++ source/scripts/player.lua
@@ -25,10 +25,14 @@ function Player:init(x, y)
 	self.yVelocity = 0
 	self.gravity = 1.0
 	self.maxSpeed = 2.0
-	self.jumpVelocity = -7
+	self.jumpVelocity = -8
+	self.drag = 0.1
+	self.minimumAirSpeed = 0.5
 
 	-- Player State
 	self.touchingGround = false
+	self.touchingCeiling = false
+	self.touchingWall = false
 end
 
 
@@ -57,6 +61,7 @@ function Player:handleState()
 			self:changeToIdleState()
 		end
 		self:applyGravity()
+		self:applyDrag(self.drag)
 		self:handleAirInput()
 	end
 end
@@ -66,10 +71,19 @@ function Player:handleMovementAndCollisions()
 	local _, _, collisions, length = self:moveWithCollisions(self.x + self.xVelocity, self.y + self.yVelocity)
 
 	self.touchingGround = false
+	self.touchingCeiling = false
+	self.touchingWall = false
+
 	for i = 1, length do
 		local collision = collisions[i]
 		if collision.normal.y == -1 then
 			self.touchingGround = true
+		elseif collision.normal.y == 1 then
+			self.touchingCeiling = true
+		end
+
+		if collision.normal.x ~= 0 then
+			self.touchingWall = true
 		end
 	end
 
@@ -132,7 +146,19 @@ end
 -- Physics Helper Functions
 function Player:applyGravity()
 	self.yVelocity = self.yVelocity + self.gravity
-	if self.touchingGround then
+	if self.touchingGround or self.touchingCeiling then
 		self.yVelocity = 0
+	end
+end
+
+function Player:applyDrag(amount)
+	if self.xVelocity > 0 then
+		self.xVelocity = self.xVelocity - amount
+	elseif self.xVelocity < 0 then
+		self.xVelocity = self.xVelocity + amount
+	end
+
+	if math.abs(self.xVelocity) < self.minimumAirSpeed or self.touchingWall then
+		self.xVelocity = 0
 	end
 end
