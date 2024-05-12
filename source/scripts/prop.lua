7da740205e2398b0f10df98908f885b6f16ba759
Charlie (Mac) <2712cgc@gmail.com>
2024-05-12 23:14:54 +0100

    Shortened Prop Names and Optimised Prop Code

--- source/scripts/prop.lua
+++ source/scripts/prop.lua
@@ -8,13 +8,13 @@ class('Prop').extends(gfx.sprite)
 --- @param x          integer The X coordinate to spawn the spike
 --- @param y          integer The Y coordinate to spawn the spike
 --- @param entityName string  The name of the entity to create as a prop
-function Prop:init(x, y, entityName)
+function Prop:init(x, y, name)
 	-- Find and open the image to use as a prop
-	local propImage <const> = gfx.image.new("images/"..entityName)
+	local image <const> = gfx.image.new("images/"..name)
 
 	self:setCenter(0, 0)
 	self:moveTo(x, y)
 	self:setZIndex(Z_INDEXES.Prop)
-	self:setImage(propImage)
+	self:setImage(image)
 	self:add()
 end
