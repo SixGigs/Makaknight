0c3e331fb4fc7abd34b7ebc830884022820bf193
Charlie (Mac) <2712cgc@gmail.com>
2024-06-03 16:08:18 +0100

    Overworld Updated

--- source/scripts/scenes/world.lua
+++ source/scripts/scenes/world.lua
@@ -61,7 +61,7 @@ function World:enterRoom(direction)
 	-- Create a local X and Y, and use them to spawn the player
 	local x, y
 	if direction == "north" then
-		x, y = self.player.x, 208
+		x, y = self.player.x, 200
 	elseif direction == "south" then
 		x, y = self.player.x, 24
 	elseif direction == "east" then
