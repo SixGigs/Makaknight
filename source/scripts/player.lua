b33a703bafde3ea24f86bb3b5f13e8c5123eb341
Charlie (Mac) <2712cgc@gmail.com>
2024-06-20 18:05:01 +0100

    Variable Jump Implemented Across Different FPS

--- source/scripts/player.lua
+++ source/scripts/player.lua
@@ -91,7 +91,7 @@ function Player:init(x, y, world, face)
 	self.yVelocity = 0
 	self.gravity = 900
 	self.maxSpeed = 135
-	self.jumpVelocity = -224
+	self.jumpVelocity = -220
 	self.minimumAirSpeed = 15
 	self.walkSpeed = 90
 	self.jumpSpeed = 115
@@ -127,14 +127,13 @@ function Player:init(x, y, world, face)
 	}
 
 	-- Variable Jump Attributes
-	self.jumpPressed = false
-	self.jumpReleased = false
+	self.jumping = false
 	self.jumpCounter = 0
-	self.jumpCounterMax = 7
+	self.jumpCounterMax = 0.2
 
 	-- Double Jump
 	self.doubleJumpAvailable = true
-	self.doubleJumpVelocity = -300
+	self.doubleJumpVelocity = -320
 
 	-- Dash
 	self.dashAvailable = true
@@ -252,19 +251,14 @@ function Player:handleState()
 				self:changeToIdleState()
 			end
 		end
-		
-		if self.jumpPressed then
-			self.jumpCounter = self.jumpCounter + (30 * dt)
-		end
-		
-		if self.jumpCounter >= self.jumpCounterMax then
+
+		if self.jumpCounter >= (self.jumpCounterMax * self.world.fps) then
 			self.jumpCounter = 0
-			self.jumpPressed = false
-			self.jumpReleased = false
+			self.jumping = false
 		end
-		
-		if self.jumpCounter ~= 0 then
-			print(self.jumpCounter)
+
+		if self.jumping then
+			self.jumpCounter = self.jumpCounter + 1
 		end
 
 		self:applyGravity()
@@ -470,8 +464,6 @@ function Player:handleGroundInput()
 			self:changeToRunState("left")
 		elseif pd.buttonIsPressed(pd.kButtonRight) then
 			self:changeToRunState("right")
-		-- elseif pd.buttonJustPressed(pd.kButtonDown) then
-		-- 	print("Pull out weapon")
 		else
 			self:changeToIdleState()
 		end
@@ -541,13 +533,12 @@ function Player:handleAirInput()
 		self:changeToDiveState()
 	end
 
-	if pd.buttonJustReleased(pd.kButtonA) and self.jumpPressed then
+	if pd.buttonJustReleased(pd.kButtonA) then
 		self.jumpCounter = 0
-		self.jumpPressed = false
-		self.jumpReleased = true
+		self.jumping = false
 	end
-	
-	if self.jumpPressed then
+
+	if self.jumping then
 		self.yVelocity = self.jumpVelocity
 	end
 end
@@ -592,8 +583,8 @@ end
 
 --- Changes the player sprite & Y velocity to the jump velocity
 function Player:changeToJumpState()
+	self.jumping = true
 	self.jumpBuffer = 0
-	self.jumpPressed = true
 	self.yVelocity = self.jumpVelocity
 	self:changeState("jump")
 end
