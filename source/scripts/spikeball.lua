ff874ba22496ea79d7bd876e0e0b90a31fc1e28d
Charlie (Mac) <2712cgc@gmail.com>
2024-07-28 19:39:37 +0100

    Flag Collision Code Reviewed and Spike Ball Code

--- source/scripts/spikeball.lua
+++ source/scripts/spikeball.lua
@@ -1,14 +1,10 @@
--- Creating the script constants
 local gfx <const> = playdate.graphics
-
--- Create the Spike ball class
 class("Spikeball").extends("Spike")
 
-
---- Initialise the spike ball object using the information given
---- @param x      integer The X coordinate to spawn the spike-ball
---- @param y      integer The Y coordinate to spawn the spike-ball
---- @param entity table   The entities that come with the spike-ball
+--- Initialise the Spike-ball object using the information given
+--- @param  x  integer  The X coordinate to spawn the Spike-ball
+--- @param  y  integer  The Y coordinate to spawn the Spike-ball
+--- @param  e  table    The entities that come with the Spike-ball
 function Spikeball:init(x, y, e)
 	Spikeball.super.init(self, x, y, e)
 
@@ -19,20 +15,29 @@ function Spikeball:init(x, y, e)
 end
 
 
---- The update method for the spike-ball, it will run every tick
-function Spikeball:update()
-	local _, _, collisions, length = self:moveWithCollisions(self.x + (self.xVelocity * dt), self.y + (self.yVelocity * dt))
-	local hitWall = false
+--- Set the collision response for the Spike-ball
+--- @param  e  table  The entity the Spike-ball collided with
+function Spikeball:collisionResponse(e)
+	local tag <const> = e:getTag()
+	if tag == TAGS.Player then
+		return gfx.sprite.kCollisionTypeOverlap
+	end
+end
 
+
+--- The update method for the Spike-ball, it will run every tick
+function Spikeball:update()
+	local _, _, collisions, length = self:moveWithCollisions(
+		self.x + (self.xVelocity * dt), 
+		self.y + (self.yVelocity * dt)
+	)
+
+	-- Bounce off walls
 	for i = 1, length do
 		local collision = collisions[i]
 		if collision.other:getTag() ~= TAGS.Player then
-			hitWall = true
+			self.xVelocity = self.xVelocity * -1
+			self.yVelocity = self.yVelocity * -1
 		end
-	end
-
-	if hitWall then
-		self.xVelocity = self.xVelocity * -1
-		self.yVelocity = self.yVelocity * -1
 	end
 end
