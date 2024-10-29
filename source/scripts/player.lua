28cd8456bd6b5cac19c3ec3d49e4ca00be2283da
Charlie (Mac) <2712cgc@gmail.com>
2024-10-29 07:55:55 +0000

    Player Passes Through Half Tiles While Diving

--- source/scripts/player.lua
+++ source/scripts/player.lua
@@ -185,7 +185,7 @@ function Player:collisionResponse(e)
 		if tag == TAGS.Fragile then
 			return e:collision(self)
 		elseif tag == TAGS.Halftile then
-			if self.y + 48 > e.y or self.currentState == 'duck' and pd.buttonIsPressed(pd.kButtonA) then
+			if self.y + 48 > e.y or self.currentState == 'duck' and pd.buttonIsPressed(pd.kButtonA) or self.yVelocity >= 750 then
 				return gfx.sprite.kCollisionTypeOverlap
 			end
 		else
