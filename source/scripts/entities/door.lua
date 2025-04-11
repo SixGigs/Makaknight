2abbfa3b4699b23ea14373046bde0d4a9e335aee
Charlie (Mac) <2712cgc@gmail.com>
2025-04-11 15:21:52 +0100

    Door collision code is now in the door object

--- source/scripts/entities/door.lua
+++ source/scripts/entities/door.lua
@@ -1,13 +1,12 @@
+local pd <const> = playdate
 local gfx <const> = playdate.graphics
-
--- Create the door class
-class("Door").extends(gfx.sprite)
+class('Door').extends(gfx.sprite)
 
 
 --- Initialise the door object using the data given
---- @param x integer The X coordinate to spawn the door
---- @param y integer The Y coordinate to spawn the door
---- @param e table   The table of entity attributes in the door
+--- @param  x  integer  The X coordinate to spawn the door
+--- @param  y  integer  The Y coordinate to spawn the door
+--- @param  e  table    The table of entity attributes in the door
 function Door:init(x, y, e)
 	-- Use entity attribute 'doorSprite' to load the correct sprite
 	local i <const> = gfx.image.new('images/doors/' .. e.name)
@@ -24,4 +23,16 @@ function Door:init(x, y, e)
 	self:setCollideRect(12, 32, 8, 16)
 	self:setImage(i)
 	self:add()
+end
+
+
+--- Used when the player collides with the door
+--- @param  player  table  The player object
+function Door:handleCollision(player)
+	if pd.buttonJustPressed(pd.kButtonUp) then
+		player.xVelocity = 0
+		player.yVelocity = 0
+		player:changeState('entering')
+		player.world:enterDoor(self)
+	end
 end
