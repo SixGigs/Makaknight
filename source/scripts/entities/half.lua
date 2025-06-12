1687b2b2574de66108eda16525e36bee535f61e8
Charlie (Mac) <2712cgc@gmail.com>
2025-06-12 14:48:29 +0100

    Cleaned Up the Drop Method in the Animal Class

--- source/scripts/entities/half.lua
+++ source/scripts/entities/half.lua
@@ -21,7 +21,7 @@ end
 --- @param  e  object  The entity that is colliding with the half tile
 function Half:collision(e)
 	-- Adds minus 0.1 for some leniency for collisions (stops player slipping through half tiles when moving the camera up & down)
-	if e.y + (e.height / 2) - 0.05 > self.y or e.currentState == 'duck' and pd.buttonIsPressed(pd.kButtonA) or e.yVelocity >= 750 then
+	if e.y + (e.height / 2) - 0.025 > self.y or e.currentState == 'duck' and pd.buttonIsPressed(pd.kButtonA) or e.yVelocity >= 750 then
 		return gfx.sprite.kCollisionTypeOverlap
 	else
 		return gfx.sprite.kCollisionTypeSlide
