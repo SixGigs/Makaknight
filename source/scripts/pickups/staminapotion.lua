c8d2dc4b9b26cddd74b75f27abdf0ca4c89b6c3e
Charlie (Mac) <2712cgc@gmail.com>
2025-06-13 21:09:19 +0100

    Took Out State Specific Code from Pickups & Added Invulnerability Frames

--- source/scripts/pickups/staminapotion.lua
+++ source/scripts/pickups/staminapotion.lua
@@ -10,33 +10,3 @@ function Staminapotion:init(x, y, ...)
 
 	Staminapotion.super.init(self, x, y, i, e)
 end
-
-
-
---- This method handles the stamina potion being picked up by an entity
---- @param  e  table  The entity colliding with the health potion
-function Staminapotion:handleCollision(e)
-	if not self:isVisible() then
-		return
-	end
-
-	local collisionTag <const> = e:getTag()
-
-	-- Collect the coin if the entity is a player
-	if collisionTag == TAGS.Player then
-		if not e.dead then
-			if e.currentState ~= 'dash' and e.currentState ~= 'dive' then
-				e.sp = e.maxSP
-
-				Sparkle(self.x + 8, self.y + 8)
-
-				if self.id then
-					GAME.depletedEntities[self.id] = true
-					self:setVisible(false)
-				else
-					self:remove()
-				end
-			end
-		end
-	end
-end
